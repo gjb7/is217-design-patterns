52c73e34d83a83e7113e484e229677dc4bb3a6dd
Grant J. Butler <butler.grant.j@gmail.com>
2014-03-24 22:26:20 -0400

    Added test for NotificationCenter.

---
+++ test/NotificationCenterSpec.js
@@ -0,0 +1,35 @@
+var NotificationCenter = require('./../lib/NotificationCenter'),
+	chai = require('chai'),
+	expect = chai.expect;
+
+var observer = function() { };
+
+describe('NotificationCenter', function() {
+	describe('#addObserver', function() {
+		it('should add an observer', function() {
+			NotificationCenter.getInstance().addObserver('event', observer);
+			
+			expect(NotificationCenter.getInstance().observers).to.have.keys('event');
+			expect(NotificationCenter.getInstance().observers.event).to.have.length(1);
+		});
+	});
+	
+	describe('#removeObserver', function() {
+		it('should remove an observer', function() {
+			NotificationCenter.getInstance().removeObserver('event', observer);
+			
+			expect(NotificationCenter.getInstance().observers).to.have.keys('event');
+			expect(NotificationCenter.getInstance().observers.event).to.have.length(0);
+		});
+	});
+	
+	describe('#post', function() {
+		it('should post a notification', function(done) {
+			NotificationCenter.getInstance().addObserver('something', function() {
+				done();
+			});
+			
+			NotificationCenter.getInstance().post('something', this);
+		});
+	});
+});
