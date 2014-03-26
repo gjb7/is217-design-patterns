8caf814cd68faf93db7e912b189af3ad9d127032
Grant J. Butler <butler.grant.j@gmail.com>
2014-03-25 21:44:11 -0400

    Implement example application.

--- patterns.js
+++ patterns.js
@@ -1,4 +1,13 @@
 var Application = require('./lib/Application'),
-	DatabaseConnectionFactory = require('./lib/DatabaseConnectionFactory');
+	DatabaseConnectionFactory = require('./lib/DatabaseConnectionFactory'),
+	MySQLDatabaseConnectionAdapter = require('./lib/db/MySQL/MySQLDatabaseConnectionAdapter'),
+	NotificationCenter = require('./lib/NotificationCenter');
+
+NotificationCenter.getInstance().addObserver(Application.LAUNCH_EVENT, function() {
+	var connection = DatabaseConnectionFactory.getInstance().make(MySQLDatabaseConnectionAdapter.identifier, ['mysql://localhost']);
+	connection.select('users', ['id', 'username']).where('uid = 1').limit(1).execute(function(rows) {
+		console.log('Found user: ' + rows[0].username);
+	});
+});
 
 Application.getInstance().run();
