95bf76aa530c8a66ab7f8b09892ee8d845ee66a3
Grant J. Butler <butler.grant.j@gmail.com>
2014-03-25 21:42:17 -0400

    Added test for Decorator pattern.

---
+++ test/DatabaseQuerySpec.js
@@ -0,0 +1,17 @@
+var DatabaseConnectionFactory = require('./../lib/DatabaseConnectionFactory'),
+	MySQLDatabaseConnectionAdapter = require('./../lib/db/MySQL/MySQLDatabaseConnectionAdapter'),
+	chai = require('chai'),
+	expect = chai.expect;
+
+describe('MySQLQuery (Decorator)', function() {
+	it('should return results', function(done) {
+		var connection = DatabaseConnectionFactory.getInstance().make(MySQLDatabaseConnectionAdapter.identifier, ['mysql://localhost']);
+		connection.select('users', ['id', 'username']).where('uid = 1').limit(1).execute(function(rows) {
+			expect(rows).to.have.length(1);
+			expect(rows[0].id).to.equal(1);
+			expect(rows[0].username).to.equal('legosjedi');
+			
+			done();
+		});
+	});
+});
