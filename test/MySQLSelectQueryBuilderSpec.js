fbb064cf4353bdd60399e2ebe53c459b6ef68df1
Grant J. Butler <butler.grant.j@gmail.com>
2014-03-25 21:11:20 -0400

    Added test for select query builder.

---
+++ test/MySQLSelectQueryBuilderSpec.js
@@ -0,0 +1,12 @@
+var MySQLSelectQueryBuilder = require('./../lib/db/MySQL/builders/MySQLSelectQueryBuilder'),
+	chai = require('chai'),
+	expect = chai.expect;
+
+describe('MySQLSelectQueryBuilder', function() {
+	it('should create a query', function() {
+		var builder = new MySQLSelectQueryBuilder('users');
+		builder.where('uid = 1').limit(1);
+		
+		expect(builder.toString()).to.equal('SELECT * FROM users WHERE uid = 1 LIMIT 1');
+	});
+});
